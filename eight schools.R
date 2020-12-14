85720d468d3ce71c8e537a5ab0469ddd22d2f4c8
mgaldino <mcz.fea@gmail.com>
2020-12-14 09:10:53 -0300

    oito escolas ajustes

--- eight schools.R
+++ eight schools.R
@@ -1,5 +1,13 @@
 library(brms)
-schools_dat <- list(J = seq(1,8,1), 
+
+
+# y_j ~ N(teta_j, se_j)
+# teta_j ~ N(mu, tau)
+
+# y_j = a_j + e_j, e_j ~ N(0, se_j)
+# a_j = mu 
+
+schools_dat <- list(J = 1:8, 
                     y = c(28,  8, -3,  7, -1,  1, 18, 12),
                     sigma = c(15, 10, 16, 11,  9, 11, 10, 18))
 
@@ -7,14 +15,21 @@ set.seed(2)
 model1 <- brm(y|se(sigma) ~ 1 + (1|J), data = schools_dat,
               iter = 3000)
 
-## alternativa
-schools <- data.frame(y = c(28,  8, -3,  7, -1,  1, 18, 12),
-                      sigma = c(15, 10, 16, 11,  9, 11, 10, 18),
-                      J = 1:8)
-
 summary(model1)
 coef(model1)$J 
+
+prior_summary(model1)
+get_prior(y|se(sigma) ~ 1 + (1|J), data = schools_dat)
+
 #to extract the summary estimates of theta and coef(<brmsmodel>, summary = FALSE)$J t
 
+set.seed(2)
+prior_tau <- set_prior("normal(0,.2)", class = "sd", coef = "Intercept", group = "J")
 
-model2 <- brm(y|se(sigma) ~ 1 + (1|J), data = schools)
+model2 <- brm(y|se(sigma) ~ 1 + (1|J), data = schools_dat,
+              iter = 5000, prior = prior_tau)
+summary(model2)
+coef(model2)$J 
+
+
+
